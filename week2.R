095a3bee4fc2f0b590660c328a9e6d156b5c6f29
Harshita Raghuvanshi - STUDENT <L00171050@atu.ie>
2023-03-14 15:29:35 +0000

    initial

--- week2.R
+++ week2.R
@@ -2,3 +2,34 @@ x <- 2
 x
 y <- 5 
 y 
+a <- b <- 7
+a
+b
+rm(a)
+a
+x <- 5
+x <- "harshita"
+class(x)
+is.numeric(x)
+is.character(x)
+i <- 5
+is.integer(i)
+class(i)
+j <- 9.789L
+j
+class(j)
+is.integer(j)
+is.double(j)
+
+a <- 4L
+b <- 5L
+d <- 4L/5L
+d
+class(d)
+nchar(x)
+x <- "data"
+y <- factor("data",levels = c("data","data1"))
+y
+class(y)
+date1 <- as.Date("2023-03-08")
+date1
